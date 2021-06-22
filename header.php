61f964757a9006bc545513e202635857073f1347
Jorge Arguelles <jorge.arguelles@outlook.com>
2021-06-22 16:37:32 -0500

    Segundo

--- header.php
+++ header.php
@@ -11,14 +11,11 @@
 	</head>
 	<body>
 		<!-- Navegacion -->
-		<nav class="navbar navbar-expand-xl navbar-dark bg-dark">
-
+		<nav class="navbar navbar-expand-xl navbar-light">
 			<div class="container-fluid">
-
 				<a href="#" class="navbar-brand">
-					<img src="recursos/imagenes/Acuantia_Masterbrand_RGB_02.png" alt="Acuantia" title="Acuantia" width="200">
+					<img src="recursos/imagenes/Acuantia_Masterbrand_RGB_02.png" alt="Logotipo Rotoplas" title="Rotoplas" width="200">
 				</a>
-
 				<button 
 					class="navbar-toggler" 
 					type="button" 
@@ -29,32 +26,22 @@
 					aria-label="Toggle navigation">
 					<span class="navbar-toggler-icon"></span>
 				</button>
-
 				<div class="collapse navbar-collapse" id="toggleMobileMenu">
-				
 					<ul class="navbar-nav ms-auto text-center align-items-center">
-						<li class="nav-item mx-xl-1"><a class="nav-link" data-i18n="[html]app-menu-about" href="#quienes-somos">¿Quiénes<br>somos?</a></li>
-						<li class="nav-item mx-xl-1"><a class="nav-link" data-i18n="[html]app-menu-services" href="#que-servicios-ofrecemos">¿Qué servicios ofrecemos?</a></li>
-						<li class="nav-item mx-xl-1"><a class="nav-link" data-i18n="[html]app-menu-history" href="#nuestra-historia">Nuestra historia</a></li>
-						<li class="nav-item mx-xl-1"><a class="nav-link" data-i18n="[html]app-menu-brands" href="#nuestras-marcas">Nuestras marcas</a></li>
-						<li class="nav-item mx-xl-1"><a class="nav-link" data-i18n="[html]app-menu-contact" href="#contacto">Contacto</a></li>
-						<!-- <li class="nav-item">
-							<a class="menu-cta btn mx-0 my-2 mx-xl-2 my-xl-0" href="#">+52 800 506 3000</a>
-						</li> -->
-						<li class="nav-item mx-xl-1 menu-regiones-btn"><a class="nav-link" data-i18n="[html]app-menu-btn-br" href="#contacto">Acuantia<br>Brasil</a></li>
-						<li class="nav-item mx-xl-1 menu-regiones-btn"><a class="nav-link" data-i18n="[html]app-menu-btn-us" href="#contacto">Acuantia<br>Estados Unidos</a></li>
-
+						<li class="nav-item mx-xl-1"><a class="nav-link" data-i18n="[html]app-menu-" href="#">Inicio</a></li>
+						<li class="nav-item mx-xl-1"><a class="nav-link" data-i18n="[html]app-menu-" href="#">Estrategia de<br>Sustentabilidad</a></li>
+						<li class="nav-item mx-xl-1"><a class="nav-link" data-i18n="[html]app-menu-" href="#">Informes<br>Anuales</a></li>
+						<li class="nav-item mx-xl-1"><a class="nav-link" data-i18n="[html]app-menu-" href="#">Pacto Muncial<br>ODS</a></li>
+						<li class="nav-item mx-xl-1"><a class="nav-link" data-i18n="[html]app-menu-" href="#">Inversionistas</a></li>
+						<li class="nav-item mx-xl-1"><a class="nav-link" data-i18n="[html]app-menu-" href="#">INNWAI</a></li>
+						<li class="nav-item mx-xl-1"><a class="nav-link" data-i18n="[html]app-menu-" href="#">A Fluir</a></li>
 						<li class="nav-item mx-xl-1">
 							<select class="form-select w-auto h-100 my-2 my-lg-0" name="languageSelector" id="languageSelector">
 								<option data-i18n="app-selector-es" value="es">Esp</option>
 								<option data-i18n="app-selector-en" value="en">Eng</option>
-								<option data-i18n="app-selector-po" value="po">Por</option>
 							</select>
 						</li>
 					</ul>
-
 				</div>
-
 			</div>
-
 		</nav>
--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 
 <div class="row">
-    <div class="col-12">Main</div>
+    <div class="col-12">Main b</div>
 </div>
 
 

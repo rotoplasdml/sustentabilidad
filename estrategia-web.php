65c5d545345d4df23883cca0bea82ab5c48f6d94
Jorge Arguelles <jorge.arguelles@outlook.com>
2021-07-01 18:20:17 -0500

    Estrategia: header, tabs

---
+++ estrategia-web.php
@@ -0,0 +1,102 @@
+<?php include 'header.php';?>
+
+<!-- Header -->
+<div class="row justify-content-md-center">
+	<div class="col-12 col-md-5 me-md-3">
+		<p class="estrategia-web-header-txt light">En 2020 comenzamos a diseñar la evolución de la estrategia actual para poder hacer el lanzamiento de la <span class="bold">Estrategia de Sustentabilidad 2021-2025</span></p>
+		<a class="rtp-btn" href="#">Estrategia 360&deg;</a>
+	</div>
+	<div class="col-12 col-md-5 ms-md-3">
+		<img src="recuros/estrategia-web/x.jpg" alt="Imagen" title="Estrategia">
+	</div>
+</div>
+
+<!-- Contenidos -->
+<div class="row justify-content-sm-center">
+	<div class="col-12 col-sm-6">
+		<p>Con esta estregia buscamos seguir avanzando en el compromso con una operación eficiente en el uso de recursos, que atiende las necesidades de los clientes facilitando el bienestar y que contribuye a un uso responsable del agua en los hogares e industrias.</p>
+	</div>
+</div>
+
+<!-- Titulo -->
+<div class="row justify-content-center">
+	<div class="col-12 text-center">
+		<p class="fs30 light lhn blue2 mt-3">Valor 360 Estrategia de Sustentabilidad</p>
+	</div>
+</div>
+
+<!-- Tabs -->
+<div class="row">
+	<div class="col d-flex flex-row justify-content-center">
+		<div class="valor-imagenes">
+			&nbsp;
+		</div>
+
+		<div class="valor-tabs-wrapper">
+			<div class="valor-tabs-botones">
+				<div class="seleccion" data="1"><p>Líneas de Acción</p></div>
+				<div data="2"><p>Contribución a Objetivos de Desarrollo Sostenible</p></div>
+				<div data="3"><p>Imacto 360&deg; Grupos de Interés</p></div>
+			</div>
+
+			<div id="valor-tabs-1" class="valor-tabs">
+				<div class="valor-tabs-nodo valor-tabs-azul">
+					<p style="background:#02b2db">1</p>
+					<ul>
+						<li>Fortaleza de la estructura y funcionamiento</li>
+						<li>Cultura de ética y cumplimiento</li>
+						<li>Integridad en la cadena de valor</li>
+					</ul>
+				</div>
+
+				<div class="valor-tabs-nodo valor-tabs-azul">
+					<p style="background:#02b2db">2</p>
+					<ul>
+						<li>Digitalización y omnicanalidad</li>
+						<li>Desarrollo de distribuidores y plomeros</li>
+					</ul>
+				</div>
+
+				<div class="valor-tabs-nodo valor-tabs-verde">
+					<p style="background:#9cc43d">3</p>
+					<ul>
+						<li>Gestión con enfoque de cliclo de vida</li>
+						<li>Aprovisionamiento sostenible</li>
+						<li>Eficiencia operacional</li>
+						<li>Compromiso climático</li>
+					</ul>
+				</div>
+
+				<div class="valor-tabs-nodo valor-tabs-verde">
+					<p style="background:#9cc43d">4</p>
+					<ul>
+						<li>Huella ambiental de cliente</li>
+						<li>Cultura del agua</li>
+						<li>Participación de la agenda pública</li>
+					</ul>
+				</div>
+
+				<div class="valor-tabs-nodo valor-tabs-rojo">
+					<p style="background:#f56c4a">5</p>
+					<ul>
+						<li>Experiencia del usuario</li>
+						<li>Alianzas para el acceso</li>
+						<li>Avance social</li>
+					</ul>
+				</div>
+
+				<div class="valor-tabs-nodo valor-tabs-rojo">
+					<p style="background:#f56c4a">6</p>
+					<ul>
+						<li>Diversidad, igualdad e inclusión</li>
+						<li>Crecimiento</li>
+						<li>Seguridad, salud y bienestar</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+
+	</div>
+</div>
+
+<?php include 'footer.php';?>
